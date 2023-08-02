3c0fc338563e231d8494c8fa9d1961a880d1a10d
Alvi <contact.ivanmilosevic@gmail.com>
2023-08-02 08:59:16 +0100

    add focus style

--- client/src/pages/LiveChat.tsx
+++ client/src/pages/LiveChat.tsx
@@ -23,7 +23,10 @@ const LiveChat = () => {
       <span className="relative z-0 inline-flex shadow-sm rounded-md py-2">
         <button
           type="button"
-          className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          className={`${
+            channel === "general" &&
+            "!bg-indigo-500 text-white !border-indigo-500 focus:!ring-0"
+          } relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500`}
           onClick={() => {
             setChannel("general");
           }}
@@ -32,7 +35,10 @@ const LiveChat = () => {
         </button>
         <button
           type="button"
-          className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          className={`${
+            channel === "spam" &&
+            "!bg-indigo-500 text-white !border-indigo-500 focus:!ring-0"
+          } -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500`}
           onClick={() => {
             setChannel("spam");
           }}
@@ -41,7 +47,10 @@ const LiveChat = () => {
         </button>
         <button
           type="button"
-          className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          className={`${
+            channel === "meme-revue" &&
+            "!bg-indigo-500 text-white !border-indigo-500 focus:!ring-0"
+          } -ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500`}
           onClick={() => {
             setChannel("meme-revue");
           }}
